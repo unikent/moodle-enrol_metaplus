4962530793b4badeeccd320c6b6ac39bb21f80c9
Skylar Kelty <s.kelty@kent.ac.uk>
2015-11-11 11:58:26 +0000

    Complete lib

--- lib.php
+++ lib.php
@@ -25,8 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . "/enrol/meta/lib.php");
-
 /**
  * Meta+ course enrolment plugin.
  *
@@ -35,7 +33,7 @@ require_once($CFG->dirroot . "/enrol/meta/lib.php");
  * @author     Skylar Kelty
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class enrol_metaplus_plugin extends enrol_meta_plugin
+class enrol_metaplus_plugin extends enrol_plugin
 {
     /**
      * Returns localised name of enrol instance
@@ -69,6 +67,81 @@ class enrol_metaplus_plugin extends enrol_meta_plugin
         return new moodle_url('/enrol/metaplus/addinstance.php', array(
             'id' => $courseid
         ));
+    }
+
+    /**
+     * Does this plugin allow manual unenrolment of a specific user?
+     * Yes, but only if user suspended...
+     *
+     * @param stdClass $instance course enrol instance
+     * @param stdClass $ue record from user_enrolments table
+     *
+     * @return bool - true means user with 'enrol/xxx:unenrol' may unenrol this user, false means nobody may touch this user enrolment
+     */
+    public function allow_unenrol_user(stdClass $instance, stdClass $ue) {
+        if ($ue->status == ENROL_USER_SUSPENDED) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Gets an array of the user enrolment actions
+     *
+     * @param course_enrolment_manager $manager
+     * @param stdClass $ue A user enrolment object
+     * @return array An array of user_enrolment_actions
+     */
+    public function get_user_enrolment_actions(course_enrolment_manager $manager, $ue) {
+        $actions = array();
+        $context = $manager->get_context();
+        $instance = $ue->enrolmentinstance;
+        $params = $manager->get_moodlepage()->url->params();
+        $params['ue'] = $ue->id;
+        if ($this->allow_unenrol_user($instance, $ue) && has_capability('enrol/meta:unenrol', $context)) {
+            $url = new moodle_url('/enrol/unenroluser.php', $params);
+            $actions[] = new user_enrolment_action(new pix_icon('t/delete', ''), get_string('unenrol', 'enrol'), $url, array(
+                'class' => 'unenrollink',
+                'rel' => $ue->id
+            ));
+        }
+
+        return $actions;
+    }
+
+    /**
+     * Called after updating/inserting course.
+     *
+     * @param bool $inserted true if course just inserted
+     * @param stdClass $course
+     * @param stdClass $data form data
+     * @return void
+     */
+    public function course_updated($inserted, $course, $data) {
+        // Do nothing.
+    }
+
+    /**
+     * Is it possible to delete enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_delete_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/meta:config', $context);
+    }
+
+    /**
+     * Is it possible to hide/show enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_hide_show_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/meta:config', $context);
     }
 
     /**
